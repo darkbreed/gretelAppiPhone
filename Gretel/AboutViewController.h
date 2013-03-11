21e7fadb2e82be2ad2f404909ba4fb1abe17b86b
Ben Reed <benreed@Bens-MacBook-Air.local>
2013-03-11 15:57:11 +0000

    fixed a few bugs, added about screen

---
+++ Gretel/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Gretel
+//
+//  Created by Ben Reed on 11/03/2013.
+//  Copyright (c) 2013 Ben Reed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
